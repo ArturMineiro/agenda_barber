44d0088eaef06a77d83dbd0f92b7738cf28db2be
Venturaa10 <joao.victor.ventura25@gmail.com>
2025-09-03 11:09:33 -0300

    - Configurar Saas backend.

--- backend/src/main/java/com/barbearia/saas_backend/SaasBackendApplication.java
+++ backend/src/main/java/com/barbearia/saas_backend/SaasBackendApplication.java
@@ -2,12 +2,16 @@ package com.barbearia.saas_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+
 
 @SpringBootApplication
 public class SaasBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaasBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SaasBackendApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
+    }
 
 }
