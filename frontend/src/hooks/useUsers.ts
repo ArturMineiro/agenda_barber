0a75c962f821ab33b5859718ea8000a0d9486380
artur.mineiro <arturmineiro2@hotmail.com>
2025-09-03 14:51:30 -0300

    configurando schemas do zod e outras configurações adicionais de estrutura

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchUsers } from "../service/UserService";
-import { usersSchema, type Users } from "../schemas/userShecma";
+import { usersSchema, type Users } from "../schemas/userSchema";
 
 export const useUsers = () => {
   return useQuery<Users>({
